62f055bf5068c706ab7e6b390c7534378e4a41f5
raphberube <raphael.berube@telus.com>
2020-10-24 17:04:46 -0400

    Simulate with zone7 weather

--- agent/simulate.py
+++ agent/simulate.py
@@ -8,7 +8,7 @@ import numpy as np
 from utils import make_dict
 
 # Create Environment. Follow the documentation of 'Gym-Eplus' to set up additional EnergyPlus simulation environment.
-env = gym.make('5Zone-sim_TMY2-v0');
+env = gym.make('7Zone-sim_TMYx-v0');
 #env = gym.make('5Zone-sim_TMY3-v0');
 
 # Modify here: Outputs from EnergyPlus; Match the variables.cfg file.
@@ -16,7 +16,7 @@ obs_name = ["Outdoor Temp.", "Outdoor RH", "Wind Speed", "Wind Direction", "Diff
 dist_name = ["Outdoor Temp.", "Outdoor RH", "Wind Speed", "Wind Direction", "Diff. Solar Rad.", "Direct Solar Rad.", "Indoor Temp. Setpoint", "Occupancy Flag"]
 
 # Reset the env (creat the EnergyPlus subprocess)
-timeStep, obs, isTerminal = env.reset();
+timeStep, obs, isTerminal = env.reset()
 obs_dict = make_dict(obs_name, obs)
 start_time = pd.datetime(year = env.start_year, month = env.start_mon, day = env.start_day)
 print(start_time)
@@ -44,9 +44,9 @@ for i in range(91*96):
 # Save Observations
 obs_df = pd.DataFrame(np.array(observations), index = np.array(timeStamp), columns = obs_name)
 dist_df = obs_df[dist_name]
-obs_df.to_pickle("results/Sim-TMY2.pkl")
+obs_df.to_pickle("results/7Zone-Sim-TMYx.pkl")
 #obs_df.to_pickle("results/Sim-TMY3.pkl")
-dist_df.to_pickle("results/Dist-TMY2.pkl")
+dist_df.to_pickle("results/7Zone-Dist-TMYx.pkl")
 #dist_df.to_pickle("results/Dist-TMY3.pkl")
 print("Saved!")
 
