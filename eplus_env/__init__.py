3f4c6af84079129cf9c3c35a361e92628fd05cbe
raphberube <raphael.berube@telus.com>
2020-10-24 17:20:59 -0400

    Adding zone 7 weather

--- eplus_env/__init__.py
+++ eplus_env/__init__.py
@@ -49,6 +49,25 @@ register(
          });
 
 
+register(
+         id='7Zone-sim_TMYx-v0',
+         entry_point='eplus_env.envs:EplusEnv',
+         kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/', # The EnergyPlus software path
+         'weather_path':FD + '/envs/weather/CAN_QC_Mont.Joli.AP.717180_TMYx.2004-2018.epw', # The epw weather file
+         'bcvtb_path':FD + '/envs/bcvtb/', # The BCVTB path
+         'variable_path':FD + '/envs/eplus_models/5Zone/variables_Default.cfg', # The cfg file
+         'idf_path':FD + '/envs/eplus_models/5Zone/SF+CZ7+USA_MN_Duluth.Intl.AP.727450+elecres+heatedbsmt+IECC_2018.idf', # The idf file
+         'env_name': '7Zone-sim-v0',
+         });
 
-
+register(
+         id='7Zone-control_TMYx-v0',
+         entry_point='eplus_env.envs:EplusEnv',
+         kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/', # The EnergyPlus software path
+         'weather_path':FD + '/envs/weather/CAN_QC_Mont.Joli.AP.717180_TMYx.2004-2018.epw', # The epw weather file
+         'bcvtb_path':FD + '/envs/bcvtb/', # The BCVTB path
+         'variable_path':FD + '/envs/eplus_models/5Zone/variables_Control.cfg', # The cfg file
+         'idf_path':FD + '/envs/eplus_models/5Zone/SF+CZ7+USA_MN_Duluth.Intl.AP.727450+elecres+heatedbsmt+IECC_2018.idf', # The idf file
+         'env_name': '7Zone-control-v0',
+         });
 
